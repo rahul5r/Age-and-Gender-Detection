cde4ae874e822368e50a04f0776ad8e1086de29e
Rahul <rahul0987654321dv@gmail.com>
2025-04-14 08:22:19 +0530

    Changes Made

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FLaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import SubmitField
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_word():
-    return render_template('index.html')
+app.config['SECRET_KEY'] = "qwerty"
+app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
 
-@app.route('/<name>')
-def rediction(name):
-    return f"<h1> Hello! {name}. </h1>"
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+
+class UploadForm(FLaskForm):
+    photo = FileField(
+        validators = [
+            FileAllowed(photos, "Only images are allowed"),
+            FileRequired("File field should not be empty")
+        ]
+    )
+    submit = SubmitField('Upload')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
+    
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
